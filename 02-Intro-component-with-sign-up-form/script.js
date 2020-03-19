2e95ff965f5e9c338c058171708d93e2a7f72a92
wale-bant <wale.ashiru04@gmail.com>
2020-03-19 13:10:10 +0100

    completed structure

---
+++ 02-Intro-component-with-sign-up-form/script.js
@@ -0,0 +1,36 @@
+const firstName = document.querySelector('.first-name');
+const lastName = document.querySelector('.last-name');
+const email = document.querySelector('.email');
+const password = document.querySelector('.password');
+const submit = document.querySelector('.btn');
+console.log(submit);
+// console.log(document.querySelector('.form-row .first-name-error'));
+const getElement = (parent, child) =>
+  document.querySelector(`.${parent} .${child}`);
+
+submit.addEventListener('click', e => {
+  e.preventDefault();
+  validate();
+});
+
+function validate() {
+  if (!firstName.value) {
+    firstName.style.border = '1.5px solid rgba(255, 0, 0, .5)';
+    getElement('form-row', 'first-name-error').style.display = 'block';
+  }
+  if (!lastName.value) {
+    lastName.style.border = '1.5px solid rgba(255, 0, 0, .5)';
+    getElement('form-row', 'last-name-error').style.display = 'block';
+  }
+  if (!email.value) {
+    email.style.border = '1.5px solid rgba(255, 0, 0, .5)';
+    getElement('form-row', 'email-error').style.display = 'block';
+  }
+  if (!password.value) {
+    password.style.border = '1.5px solid rgba(255, 0, 0, .5)';
+    getElement('form-row', 'password-error').style.display = 'block';
+  }
+
+  if (firstName.value || lastName.value || email.value || password.value)
+    submit.disabled = false;
+}
